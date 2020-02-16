9f35657cf3bbd15a8c3673d463379bd61424ae20
Capybara Translation <capybara.translation@gmail.com>
2020-02-16 16:36:14 +0900

    Add accounts app

--- superlists/settings.py
+++ superlists/settings.py
@@ -45,13 +45,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
-    'accounts',
+    # 'accounts',
 ]
 
-AUTH_USER_MODEL = 'accounts.ListUser'
-AUTHENTICATION_BACKENDS = [
-    'accounts.authentication.PasswordlessAuthenticationBackend',
-]
+# AUTH_USER_MODEL = 'accounts.ListUser'
+# AUTHENTICATION_BACKENDS = [
+#     'accounts.authentication.PasswordlessAuthenticationBackend',
+# ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -136,3 +136,20 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_TEST_TO = os.environ.get('EMAIL_TEST_TO')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
