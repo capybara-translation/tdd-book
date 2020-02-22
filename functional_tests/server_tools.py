7557039444d6ccdb17fbebda09bff88ffcd28e89
Capybara Translation <capybara.translation@gmail.com>
2020-02-22 18:22:57 +0900

    update

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -7,17 +7,29 @@ def _get_manage_dot_py(host):
     return f'~/sites/{host}/virtualenv/bin/python ~/sites/{host}/manage.py'
 
 
+def _get_hostname(ssh_config_path):
+    with open(ssh_config_path) as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith('Host '):
+                return line.replace('Host ', '')
+        return None
+
+
 def reset_database(ssh_config_path):
     config = Config(
         runtime_ssh_path=ssh_config_path
     )
-    manage_dot_py = _get_manage_dot_py(config.host)
-    with Connection(host=config.name, config=config) as c:
+    host = _get_hostname(ssh_config_path)
+    with Connection(host=host, config=config) as c:
+        manage_dot_py = _get_manage_dot_py(c.host)
         c.run(f'{manage_dot_py} flush --noinput')
 
 
 def _get_server_env_vars(c, host):
-    env_lines = c.run(f'cat ~/sites/{host}/.env').splitlines()
+    env_lines = c.run(
+        f'cat ~/sites/{host}/.env',
+        hide=True).stdout.strip().splitlines()
     return dict(l.split('=') for l in env_lines if l)
 
 
@@ -25,12 +37,12 @@ def create_session_on_server(ssh_config_path, email):
     config = Config(
         runtime_ssh_path=ssh_config_path
     )
-    manage_dot_py = _get_manage_dot_py(config.host)
-    with Connection(host=config.name, config=config) as c:
-        env_vars = _get_server_env_vars(c, config.host)
+    host = _get_hostname(ssh_config_path)
+    with Connection(host=host, config=config) as c:
+        manage_dot_py = _get_manage_dot_py(c.host)
+        env_vars = _get_server_env_vars(c, c.host)
         c.config.run.env = env_vars
         session_key = c.run(
             f'{manage_dot_py} create_session {email}',
             hide=True).stdout.strip()
-        print(session_key)
         return session_key
